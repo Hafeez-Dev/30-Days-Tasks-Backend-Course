db6b0a6e042818680a023aa4b65878e21d8e7830
Abdul Hafeez <debugerxy@gmail.com>
2025-05-29 12:47:51 +0500

    Implement Todo management features with TodoForm and TodoItem components

--- Task 3/src/App.jsx
+++ Task 3/src/App.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
-import { TodoProvider } from './context/TodoContext'
+import React, {useState} from "react";
+import { TodoProvider } from "./context/TodoContext";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = (todo) => {
-        const id = Date.now();
-        setTodos((prevTodos) => [{id, ...todo}, ...prevTodos])
-    }
+  const addTodo = (todo) => {
+    const id = Date.now();
+    setTodos((prevTodos) => [{ id, ...todo }, ...prevTodos]);
+  };
 
-    const updateTodo = (id, todo) => {
-        setTodos((prevTodos) => prevTodos.map() )
-    }
+  const updateTodo = (id, todo) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((currTodo) => (currTodo.id === id ? todo : currTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
   return (
-    <>
-      
-    </>
-  )
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
